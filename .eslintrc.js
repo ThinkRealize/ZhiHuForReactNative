29f52d2d6b23abdd9fc2db8cd841494ad457fd0a
黄龙源 <hly@spacetech.com.cn>
2019-03-16 19:18:12 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    'react-native/react-native': true
+  },
+  extends: ['standard', 'plugin:jsx-a11y/recommended'],
+
+  // required to lint *.vue files
+  plugins: ['react', 'react-native', 'jsx-a11y'],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    'space-before-function-paren': 'off',
+    /**
+     * 修复 no-unused-vars 无法识别将导入的模块用作jsx组件
+     * https://github.com/eslint/eslint/issues/8226#issuecomment-285683085
+     */
+    'react/jsx-uses-vars': 'error'
+  }
+}
